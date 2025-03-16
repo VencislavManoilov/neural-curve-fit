ec986dd3eb669ee1ab4301825f61db6a5b9fc00a
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2025-03-16 20:31:02 +0200

    😋 Enhance device information output for CUDA and CPU

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
+import platform
 # from IPython.display import clear_output
 
 # Use CUDA if available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
-print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0))
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+else:
+    print(platform.processor())
 
 # Generate sine wave data
 x = torch.linspace(-2 * np.pi, 2 * np.pi, 1000).view(-1, 1).to(device)
